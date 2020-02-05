a9ce49058a29eedbeb86e67ac66fcd9e8018219f
Morten Brøns-Pedersen <f@ntast.dk>
2020-02-05 15:40:41 +0100

    .

--- .config/fish/functions/git-export.fish
+++ .config/fish/functions/git-export.fish
@@ -1,7 +1,9 @@
 function git-export -d "Export all versions of file or directory"
 
-  set -l repo=(git rev-parse --show-toplevel)
-  if $status ;
+  set -l repo (git rev-parse --show-toplevel)
+  if [ $status -ne 0 ] ;
     echo "Not in a git repository"
   end
+
+  # TODO
 end
